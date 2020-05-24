779a779b5f8a9babf96b73570a945dfc652b75bb
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-05-25 00:05:32 +0400

    Added Burger Builder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AUX from "../../hoc/hoc-aux";
+import classes from "./Layout.css";
 
 const Layout = (props) => {
   return (
     <AUX>
       <div>ToolBar, SideDrawer, BackDrop</div>
-      <main>{props.children}</main>
+      <main className={classes.Content}>{props.children}</main>
     </AUX>
   );
 };
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import AUX from "../../hoc/hoc-aux";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <AUX>
+        <div>Burger</div>
+        <div>BurgerBuilder</div>
+      </AUX>
+    );
+  }
+}
+
+export default BurgerBuilder;
